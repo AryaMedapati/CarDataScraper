a19fe3ae0483e7389cc5a4be3f7360ed4e1d54cf
AryaMedapati <42080625+AryaMedapati@users.noreply.github.com>
2023-05-13 01:18:19 +0000

    Debug Scraper for Last Page

--- car_scraper.py
+++ car_scraper.py
@@ -20,16 +20,14 @@ with open('car_data.csv', mode='w', newline='') as file:
             middle_url = '/'
         end_url = '?pricerange=0-30000&years=2012-2024'
         url = start_url + middle_num + middle_url + end_url
-        response = requests.get(url)
-        #print(response.text)
+        response = requests.get(url, 1.0)
         soup = BeautifulSoup(response.text, 'html.parser')
-        #print(soup)
         results = soup.find('div', class_="css-a21061 e1qqueke1")
+        print("Results:", results)
         counter = 0
-        specounter = 0
-        
+        specounter = 0      
         for result in results:
-            print(counter)
+            # print(counter)
             # print("Results:", result)
             # print("NEWLINE")
             counter = counter + 1
@@ -37,9 +35,9 @@ with open('car_data.csv', mode='w', newline='') as file:
             specounter = 0
             for specific in specifics:
                 specounter = specounter + 1
-                print("counter:", specounter)
-                print("NEWLINE")
-                print(specific)
+                # print("counter:", specounter)
+                # print("NEWLINE")
+                # print(specific)
                 print("EARLYCOUNTER:", counter)
                 if specounter == 2 and counter != 2:
                     title = specific.text
@@ -53,7 +51,7 @@ with open('car_data.csv', mode='w', newline='') as file:
 
                     mileages = specific.find('div', class_='css-14q4cew e19qstch13')
                     for child in mileages.descendants:
-                        if 'MPG' in child.text and len(child.text) < 8:
+                        if 'MPG' in child.text and len(child.text) < 8 or 'N/A' in child.text:
                             mileage = child
 
                     my_mileage = mileage.text
@@ -61,23 +59,27 @@ with open('car_data.csv', mode='w', newline='') as file:
                     my_mileage = my_mileage.replace(' ', '')
                     print("MILEAGE:", my_mileage)
 
-                    # mpg = specific.find('div', class_="css-fpbjth e151py7u1")
-
                     ratings = specific.find('div', class_='css-1ouitaz ex4y58i1')
                     expert_rating = ratings.contents[0]
                     my_expert = expert_rating.text
                     my_expert = my_expert.replace('Expert', '')
                     my_expert = my_expert.replace(' ', '')
+                    my_expert = my_expert.replace('(', '')
+                    my_expert = my_expert.replace(')', '')
                     print("EXPERT RATING:", my_expert)
 
                     consumer_rating = ratings.contents[1]
                     my_consumer = consumer_rating.text
                     my_consumer = my_consumer.replace('Consumer', '')
                     my_consumer = my_consumer.replace(' ', '')
+                    my_consumer = my_consumer.replace('(', '')
+                    my_consumer = my_consumer.replace(')', '')
                     print("CONSUMER RATING:", my_consumer)
                     writer.writerow([title, my_price, my_mileage, my_expert, my_consumer])
 
         new_results = soup.find('div', class_='css-8wywm8 e1qqueke1')
+        if new_results is None:
+            new_results = soup.find('div', class_="css-269gm9 e1qqueke1")
         counter = 0
         specounter = 0
         for result in new_results:
@@ -89,10 +91,11 @@ with open('car_data.csv', mode='w', newline='') as file:
             specounter = 0
             for specific in specifics:
                 specounter = specounter + 1
-                print("counter:", specounter)
+                print("NEW SPECIFIC")
+                # print("counter:", specounter)
                 print("NEWLINE")
                 print(specific)
-                print("EARLYCOUNTER:", counter)
+                # print("EARLYCOUNTER:", counter)
                 if specounter == 2 and counter != 2:
                     title = specific.text
                     print("TITLE:", title)
@@ -114,25 +117,22 @@ with open('car_data.csv', mode='w', newline='') as file:
                     my_mileage = my_mileage.replace(' ', '')
                     print("MILEAGE:", my_mileage)
 
-                    # mpg = specific.find('div', class_="css-fpbjth e151py7u1")
-
                     ratings = specific.find('div', class_='css-1ouitaz ex4y58i1')
                     expert_rating = ratings.contents[0]
                     my_expert = expert_rating.text
                     my_expert = my_expert.replace('Expert', '')
                     my_expert = my_expert.replace(' ', '')
+                    my_expert = my_expert.replace('(', '')
+                    my_expert = my_expert.replace(')', '')
                     print("EXPERT RATING:", my_expert)
 
                     consumer_rating = ratings.contents[1]
                     my_consumer = consumer_rating.text
                     my_consumer = my_consumer.replace('Consumer', '')
                     my_consumer = my_consumer.replace(' ', '')
+                    my_consumer = my_consumer.replace('(', '')
+                    my_consumer = my_consumer.replace(')', '')
                     print("CONSUMER RATING:", my_consumer)
                     writer.writerow([title, my_price, my_mileage, my_expert, my_consumer])
-    # print(my_title)
-    # print(my_price)
-    # print(my_mileage)
-    # print(my_expert)
-    # print(my_consumer)
 
 
